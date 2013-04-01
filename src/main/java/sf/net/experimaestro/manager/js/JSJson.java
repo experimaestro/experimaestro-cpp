741c88e8fff84f9f76c9e7007e551017b51f54c7
Benjamin Piwowarski <benjamin@bpiwowar.net>
2013-04-01 22:04:39 +0200

    Compiles

---
+++ src/main/java/sf/net/experimaestro/manager/js/JSJson.java
@@ -0,0 +1,8 @@
+package sf.net.experimaestro.manager.js;
+
+/**
+ * @author B. Piwowarski <benjamin@bpiwowar.net>
+ * @date 1/4/13
+ */
+public class JSJson {
+}
---
+++ src/main/java/sf/net/experimaestro/manager/json/JsonString.java
@@ -0,0 +1,8 @@
+package sf.net.experimaestro.manager.json;
+
+/**
+ * @author B. Piwowarski <benjamin@bpiwowar.net>
+ * @date 1/4/13
+ */
+public class JsonString {
+}
