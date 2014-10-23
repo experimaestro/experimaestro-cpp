6104fc7d0c88edcf53c2eec8c1e53e314151edc1
Benjamin Piwowarski <benjamin@bpiwowar.net>
2014-10-23 18:37:20 +0200

    Added forgotten file

---
+++ server/src/main/java/sf/net/experimaestro/manager/js/XPMContext.java
@@ -0,0 +1,184 @@
+package sf.net.experimaestro.manager.js;
+
+import bpiwowar.argparser.utils.Introspection;
+import org.apache.log4j.Hierarchy;
+import org.eclipse.wst.jsdt.debug.rhino.debugger.RhinoDebugger;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+import sf.net.experimaestro.manager.Manager;
+import sf.net.experimaestro.manager.Repositories;
+import sf.net.experimaestro.manager.plans.Constant;
+import sf.net.experimaestro.scheduler.Command;
+import sf.net.experimaestro.scheduler.ResourceLocator;
+import sf.net.experimaestro.scheduler.Scheduler;
+import sf.net.experimaestro.utils.Cleaner;
+import sf.net.experimaestro.utils.JSUtils;
+import sf.net.experimaestro.utils.log.Logger;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Map;
+
+/**
+ * Global context when executing a javascript
+ */
+public class XPMContext implements AutoCloseable {
+    static private Scriptable XPM_SCOPE;
+
+    static private final Logger LOGGER = Logger.getLogger();
+
+    /**
+     * The global functions
+     */
+    static final JSUtils.FunctionDefinition[] definitions = {
+            new JSUtils.FunctionDefinition(XPMObject.class, "qname", Object.class, String.class),
+            new JSUtils.FunctionDefinition(XPMObject.class, "include"),
+            new JSUtils.FunctionDefinition(XPMObject.class, "include_repository"),
+            new JSUtils.FunctionDefinition(XPMObject.class, "script_file"),
+            new JSUtils.FunctionDefinition(XPMObject.class, "xpath", String.class, Object.class),
+            new JSUtils.FunctionDefinition(XPMObject.class, "path"),
+            new JSUtils.FunctionDefinition(XPMObject.class, "value"),
+            new JSUtils.FunctionDefinition(XPMObject.class, "file"),
+            new JSUtils.FunctionDefinition(XPMObject.class, "format"),
+            new JSUtils.FunctionDefinition(XPMObject.class, "unwrap", Object.class),
+            new JSUtils.FunctionDefinition(XPMObject.class, "set_workdir"),
+    };
+
+    private final RhinoDebugger debugger;
+    private final Context context;
+    private final Map<String, String> environment;
+    private final Repositories repositories;
+    private final Scheduler scheduler;
+    private final Hierarchy loggerRepository;
+
+    /**
+     * Get the scope where all the main objects are defined
+     */
+    public static Scriptable init() throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+        if (XPM_SCOPE == null) {
+            ContextFactory factory = new ContextFactory();
+            Context context = factory.enterContext();
+            // Sealed object
+            XPM_SCOPE = context.initStandardObjects(null, true);
+
+
+            // --- Define functions and classes
+
+            // Define the new classes (scans the package for implementations of ScriptableObject)
+            ArrayList<Class<?>> list = new ArrayList<>();
+
+            try {
+                final String packageName = XPMObject.class.getPackage().getName();
+                final String resourceName = packageName.replace('.', '/');
+                final Enumeration<URL> urls = XPMObject.class.getClassLoader().getResources(resourceName);
+                while (urls.hasMoreElements()) {
+                    URL url = urls.nextElement();
+                    Introspection.addClasses(
+                            aClass -> (ScriptableObject.class.isAssignableFrom(aClass)
+                                    || JSConstructable.class.isAssignableFrom(aClass)
+                                    || JSBaseObject.class.isAssignableFrom(aClass))
+                                    && ((aClass.getModifiers() & Modifier.ABSTRACT) == 0), list, packageName, -1, url);
+                }
+            } catch (IOException e) {
+                LOGGER.error(e, "While trying to grab resources");
+            }
+
+            // Add the classes to javascript
+            for (Class<?> aClass : list) {
+                JSBaseObject.defineClass(XPM_SCOPE, aClass);
+            }
+
+            JSBaseObject.defineClass(XPM_SCOPE, Command.class);
+            JSBaseObject.defineClass(XPM_SCOPE, Constant.class);
+
+            // Add global functions
+            for (JSUtils.FunctionDefinition definition : definitions)
+                JSUtils.addFunction(XPM_SCOPE, definition);
+
+            // namespace
+            addNewObject(context, XPM_SCOPE, "xp", "Namespace", new Object[]{"xp", Manager.EXPERIMAESTRO_NS});
+
+            // Adds a Pipe object
+            XPMContext.addNewObject(context, XPM_SCOPE, "PIPE", "Pipe", new Object[]{});
+
+            context.exit();
+        }
+        return XPM_SCOPE;
+    }
+
+    public XPMContext(Map<String, String> environment, Repositories repositories, Scheduler scheduler, Hierarchy loggerRepository, Integer debugPort) throws Exception {
+        this.repositories = repositories;
+        this.scheduler = scheduler;
+        this.loggerRepository = loggerRepository;
+        // --- Debugging via JSDT
+        // http://wiki.eclipse.org/JSDT/Debug/Rhino/Embedding_Rhino_Debugger#Example_Code
+        ContextFactory factory = new ContextFactory();
+
+        if (debugPort != null) {
+            debugger = new RhinoDebugger("transport=socket,suspend=y,address=" + debugPort);
+            debugger.start();
+            factory.addListener(debugger);
+        } else {
+            debugger = null;
+        }
+        context = factory.enterContext();
+
+        this.environment = environment;
+
+    }
+
+    static void addNewObject(Context cx, Scriptable scope,
+                             final String objectName, final String className,
+                             final Object[] params) {
+        ScriptableObject.defineProperty(scope, objectName, cx.newObject(scope, className, params), 0);
+    }
+
+    @Override
+    public void close() throws Exception {
+        // Stop debugger
+        if (debugger != null)
+            try {
+                debugger.stop();
+            } catch (Exception e) {
+                LOGGER.error(e);
+            }
+        // Exit context
+        context.exit();
+
+    }
+
+    public Object evaluateReader(ResourceLocator locator, FileReader reader, String filename, int lineno, Object security) throws Exception {
+        try(Cleaner cleaner = new Cleaner()) {
+            XPMObject xpmObject = getXPMObject(locator, cleaner);
+            XPMObject.threadXPM.set(xpmObject);
+            return context.evaluateReader(xpmObject.scope, reader, filename, lineno, security);
+        }
+    }
+
+    public Object evaluateString(ResourceLocator locator, String content, String filename, int lineno, Object security) throws Exception {
+        try(Cleaner cleaner = new Cleaner()) {
+            XPMObject xpmObject = getXPMObject(locator, cleaner);
+            XPMObject.threadXPM.set(xpmObject);
+            return context.evaluateString(xpmObject.scope, content, filename, lineno, security);
+        }
+    }
+
+    private XPMObject getXPMObject(ResourceLocator locator, Cleaner cleaner) throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+        return new XPMObject(locator, context, environment, newScope(), repositories, scheduler, loggerRepository, cleaner, null);
+    }
+
+    public static Scriptable newScope() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Scriptable newScope = Context.getCurrentContext().newObject(init());
+        newScope.setPrototype(XPM_SCOPE);
+        newScope.setParentScope(null);
+
+        return newScope;
+    }
+}
