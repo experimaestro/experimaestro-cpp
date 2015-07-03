3098cc73de2738c0cf3272b6c9b685947bff7dd5
Benjamin Piwowarski <benjamin@bpiwowar.net>
2015-07-03 11:58:29 +0200

    Use requirements.txt to add the markdown plugin

---
+++ docs/mdx/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mdx_xpm',
+      version='1.0',
+      description='Markdown extension plugin for multiple code blocks',
+      author='Benjamin Piwowarski',
+      author_email='benjamin@bpiwowar.net',
+      py_modules = ["mdx_xpm"]
+     )
