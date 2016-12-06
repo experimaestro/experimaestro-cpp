dd2086d493bd626d5bffb6513c88726d5c28ae45
Benjamin Piwowarski <benjamin@bpiwowar.net>
2016-12-06 21:29:25 +0100

    Fixed some tests

--- cpplib/test/digest.cpp
+++ cpplib/test/digest.cpp
@@ -1,39 +1,32 @@
 //
-// Created by Benjamin Piwowarski on 06/12/2016.
+// Digest-related test on structured values
 //
 
-//#include <xpm/json.hpp>
 #include <xpm/xpm.h>
-
 #include <gtest/gtest.h>
 
 //using nlohmann::json;
 using namespace xpm;
 
 TEST(Digest, Same) {
-  StructuredValue
-      v0(R"({ "a":  01  })"),
-      v1(R"({ "a": 1 }))");
-  EXPECT_EQ(v0.uniqueIdentifier(), v1.uniqueIdentifier());
+  StructuredValue::Ptr v0 = StructuredValue::parse(R"({ "a":  1  })");
+  StructuredValue::Ptr v1 = StructuredValue::parse(R"({ "a": 1 })");
+  EXPECT_EQ(v0->uniqueIdentifier(), v1->uniqueIdentifier());
 }
 
 TEST(Digest, Different) {
-  StructuredValue
-      v0(R"({ "a": 1 })"),
-      v1(R"({ "a": 2 }))");
-  EXPECT_NE(v0.uniqueIdentifier(), v1.uniqueIdentifier());
+  StructuredValue::Ptr v0 = StructuredValue::parse(R"({ "a": 1 })");
+  StructuredValue::Ptr v1 = StructuredValue::parse(R"({ "a": 2 })");
+  EXPECT_NE(v0->uniqueIdentifier(), v1->uniqueIdentifier());
 }
 TEST(Digest, IgnoredKey) {
-  StructuredValue
-      v0(R"({"$path": 1, "a": 1})"),
-      v1(R"({ "a": 1 }))");
-  EXPECT_EQ(v0.uniqueIdentifier(), v1.uniqueIdentifier());
+  StructuredValue::Ptr v0 = StructuredValue::parse(R"({"$path": 1, "a": 1})");
+  StructuredValue::Ptr v1 = StructuredValue::parse(R"({ "a": 1 })");
+  EXPECT_EQ(v0->uniqueIdentifier(), v1->uniqueIdentifier());
 }
 
-
 TEST(Digest, subkeys) {
-  StructuredValue
-      v0(R"({ "a": 1})"),
-      v1(R"({ "a": { "$value": 2, "z": "ignore" } }))");
-  EXPECT_EQ(v0.uniqueIdentifier(), v1.uniqueIdentifier());
+  StructuredValue::Ptr v0 = StructuredValue::parse(R"({ "a": 1})");
+  StructuredValue::Ptr v1 = StructuredValue::parse(R"({ "a": { "$value": 2, "z": "ignore" } })");
+  EXPECT_EQ(v0->uniqueIdentifier(), v1->uniqueIdentifier());
 }
