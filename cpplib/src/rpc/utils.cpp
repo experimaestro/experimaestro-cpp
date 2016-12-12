49d9f0fce7d45f81ec3110e87c967e73cc42599f
Benjamin Piwowarski <benjamin@bpiwowar.net>
2016-12-12 07:46:05 +0100

    Added logging to C++ library

--- cpplib/src/rpc/utils.cpp
+++ cpplib/src/rpc/utils.cpp
@@ -4,10 +4,15 @@
 
 #include <xpm/rpc/utils.hpp>
 #include <xpm/rpc/client.hpp>
+#include <spdlog/spdlog.h>
 
 namespace xpm {
 namespace rpc {
 using nlohmann::json;
+
+namespace {
+  auto LOGGER = spdlog::stdout_color_mt("rpc");
+}
 
 json ServerObject::__call__(std::string const &name, json &params) {
   params["__this__"] = _identifier;
@@ -19,7 +24,7 @@ json ServerObject::__call__(std::string const &name, json &params) {
 }
 
 json ServerObject::__static_call__(std::string const &name, json const &params) {
-  std::cerr << "[rpc call] " << name << std::endl;
+  LOGGER->info("rpc call {}", name);
   auto response = Client::defaultClient().call(name, params);
   if (response.error()) {
     throw std::runtime_error("Error with RPC call: " + response.errorMessage());
@@ -36,6 +41,7 @@ ServerObject::ServerObject() : _identifier(-1) {
 
 ServerObject::~ServerObject() {
   if (_identifier >= 0) {
+    LOGGER->info("Deleting object {}", _identifier);
     auto response = Client::defaultClient().call("objects.__delete__", {{"__this__", _identifier}});
     if (response.error()) {
       std::cerr << "Error while destroying object on server: " << response.errorMessage() << std::endl;
