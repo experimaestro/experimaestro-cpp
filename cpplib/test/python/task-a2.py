39d9c706c1c970485ff7fe1c96bf39faa4d04857
Benjamin Piwowarski <benjamin@bpiwowar.net>
2016-12-12 17:44:47 +0100

    Added progress code

--- cpplib/test/python/task-a2.py
+++ cpplib/test/python/task-a2.py
@@ -1,9 +1,13 @@
 from test import A2 as A2
-from experimaestro import register, MergeClass
+from experimaestro import register, MergeClass, progress
+import time
 
 @MergeClass(A2)
 class _A2:
     def execute(self):
+        for i in range(20):
+            time.sleep(1)
+            progress((i+1)/20)
         print("Hello world %s" % (self.seed))
 
 register.parse()
