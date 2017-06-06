02f01e485ed6ff84decbfc6730ab90936bdc32b8
Benjamin Piwowarski <benjamin@bpiwowar.net>
2017-06-06 15:46:39 +0200

    Fixed issue with YAML library

--- cpplib/cmake/FindYamlCPP.cmake
+++ cpplib/cmake/FindYamlCPP.cmake
@@ -6,11 +6,11 @@
 #  LIBYAMLCPP_DEFINITIONS - Compiler switches required for using libyaml-cpp
 
 find_package(PkgConfig)
-pkg_check_modules(PC_LIBYAMLCPP QUIET libyaml-cpp)
+pkg_check_modules(PC_LIBYAMLCPP QUIET yaml-cpp)
 set(LIBYAMLCPP_DEFINITIONS ${PC_LIBYAMLCPP_CFLAGS_OTHER})
 
-message(STATUS "Looking for libyaml-cpp...")
-find_path(LIBYAMLCPP_INCLUDE_DIR libyaml-cpp/yaml.h
+message(STATUS "Looking for yaml-cpp...")
+find_path(LIBYAMLCPP_INCLUDE_DIR yaml-cpp/yaml.h
         HINTS ${PC_LIBYAMLCPP_INCLUDEDIR} ${PC_LIBYAMLCPP_INCLUDE_DIRS}
         )
 
@@ -23,7 +23,7 @@ set(LIBYAMLCPP_INCLUDE_DIRS ${LIBYAMLCPP_INCLUDE_DIR} )
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBYAMLCPP_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(LibSsh  DEFAULT_MSG
+find_package_handle_standard_args(LIBYAMLCPP  DEFAULT_MSG
         LIBYAMLCPP_LIBRARY LIBYAMLCPP_INCLUDE_DIR)
 
 mark_as_advanced(LIBYAMLCPP_INCLUDE_DIR LIBYAMLCPP_LIBRARY)
