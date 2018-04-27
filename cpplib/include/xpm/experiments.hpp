87ebd3810cdb76beba50908d0da61d0f36207d7d
Benjamin Piwowarski <benjamin@piwowarski.fr>
2018-04-27 13:37:59 +0200

    Cleanup

---
+++ cpplib/include/xpm/experiments.hpp
@@ -0,0 +1,20 @@
+/**
+ * This header contains methods and classes related to experiment management.
+ */
+
+#ifndef XPM_EXPERIMENTS_H
+#define XPM_EXPERIMENTS_H
+
+namespace xpm {
+
+/**
+ * An experiment is characterized by a name, a timestamp, 
+ * and a list of tasks
+ */
+class Experiment {
+    Experiment(std::string const &name);
+};
+
+}
+
+#endif
--- cpplib/src/cpp.cpp
+++ cpplib/src/cpp.cpp
@@ -2,7 +2,7 @@
 // Created by Benjamin Piwowarski on 13/01/2017.
 //
 
-#include "cpp.hpp"
+#include <xpm/cpp.hpp>
 
 
 namespace xpm {
