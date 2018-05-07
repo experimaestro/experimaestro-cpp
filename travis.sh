ca35a7cf666fd9574eb36a7ecfc472bfb98d38b4
Benjamin Piwowarski <benjamin@piwowarski.fr>
2018-05-07 16:29:02 +0200

    Travis fixes

--- travis.sh
+++ travis.sh
@@ -16,5 +16,11 @@ log "cmake version $(cmake --version)"
 mkdir build
 cd build || exit 1
 
+log "--- Configuring"
 CXX=g++-7 CC=gcc-7 cmake -C ..
+
+log "--- Building"
+make
+
+log "--- Running tests"
 make experimaestro-tests
