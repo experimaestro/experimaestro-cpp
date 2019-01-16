7a9446eff7d525b9f2d3e60a52ae854b1b05bd2a
Benjamin Piwowarski <benjamin@piwowarski.fr>
2019-01-16 18:47:08 +0100

    Removed OAR docker

--- test/docker/run.sh
+++ test/docker/run.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 
-# # Wait for MySQL to be up
-# export PGPASSWORD=password
-# while ! psql  -h postgres -U admin  -c "select 'It is running'" | grep -q "It is running"; do sleep 1; done
-
-# # Create OAR database
-# oar-database --create  --db-host=postgres --db-user=oar --db-pass=oar --db-ro-user=oar_ro --db-ro-pass=oar_ro --db-admin-user=admin --db-admin-pass=password --db-port=5432 --db-type=Pg
-
-# Add node
-# service oar-server start
+# Start ssh
 service ssh start
-
-# Add resources
-# echo yes  | oar_resources_init  ~oar/oar.hosts
-# source /tmp/oar_resources_init.cmd
 
 # And then... wait!
 sleep infinity
--- test/docker/start-oar-node.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir -p /var/run/sshd
-
-/usr/sbin/sshd -f /etc/oar/sshd_config  -D
